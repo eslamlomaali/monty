e1d7d0a8ecdafd7ca41694a829edc60da3ab76f5
Youssef <youssef@email.com>
2023-11-16 22:31:58 +0200

    LIFO, FIFO

---
+++ pchar.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+/**
+ * print_chr - prints the char at the top of the stack, followed by a new line.
+ * @listss: stack
+ * @line_number: instructions' nums
+ * Return: void
+*/
+void print_chr(stack_t **listss, unsigned int line_number)
+{
+	stack_t *fifthtwice, *sixthtwice;
+
+	if (*listss == NULL || (*listss)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
+		let_m(NULL, NULL, 'r');
+		free_st(*listss);
+		free(helper.point);
+		exit(EXIT_FAILURE);
+	}
+	if (listss[0]->n < 32 || listss[0]->n > 126)
+	{
+		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
+		let_m(NULL, NULL, 'r');
+		free_st(*listss);
+		free(helper.point);
+		exit(EXIT_FAILURE);
+	}
+	printf("%c\n", listss[0]->n);
+
+}
